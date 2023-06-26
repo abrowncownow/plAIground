dff18a4ee8dbf2d924d7dc7cfa89e01a4fc7fa4c
Alex Brown <abrowncownow@localhost.localdomain>
2023-06-26 00:34:06 -0700

    it works

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import { Configuration, OpenAIApi } from "openai";
 import { config } from 'dotenv';
 config();
+
+const configuration = new Configuration({
+    organization: "org-owR7h7j93sgLxZWl7VoWsNAD",
+    apiKey: process.env.OPENAI_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.listModels();
+console.log(response.data);
