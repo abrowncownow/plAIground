bde3f9db8e8ece156f84c08495eb27261615ee1c
Alex Brown <abrowncownow@gmail.com>
2023-06-28 19:10:58 -0700

    got it working

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { config } from 'dotenv';
 config();
-import { get_completion } from "./helper/chatComplete3-5-turbo";
 
 const configuration = new Configuration({
     organization: "org-owR7h7j93sgLxZWl7VoWsNAD",
@@ -11,4 +10,27 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 // const response = await openai.listModels();
 // console.log(response.data);
+
+async function get_completion(prompt, model="gpt-3.5-turbo") {
+    const messages = [{"role": "user", "content": prompt}];
+    const response = await openai.createChatCompletion({
+      model: model,
+      messages: messages,
+      temperature: 0
+    });
+    const result = response.data.choices[0].message["content"];
+    console.log(result);
+    return result;
+  }
+
+
+const text = "Japanese Literature";
+
+const prompt = `Generate a list of made up movie titles along with their directors and the genre based on the provided subject. Provide them in JSON format with the following keys:
+movie_id, title, author, genre, leading_actors
+
+subject: ${text}`;
+
+const movies = get_completion(prompt);
+console.log(movies);
 
